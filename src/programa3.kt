48d8b629d3b255df63bd0aca36b6d8e99f38f764
Pelayo García Menéndez <pelayogm@informatica.cifpaviles.pa>
2024-11-25 19:42:32 +0100

    tercer programa - expresiones balanceadas - Pelayogm

---
+++ src/programa3.kt
@@ -0,0 +1,55 @@
+fun main () {
+    println("Introduce una funcion:")
+    var funcion = readln()
+    comprobarFunciones(funcion)
+}
+
+fun comprobarFunciones (funcion:String) {
+    var funcionArray = funcion.toCharArray()
+    var delimitadores: MutableList<Boolean> = mutableListOf()
+    var estaBalanceada = true
+    for (i in funcionArray.indices) {
+        when (funcionArray[i]) {
+            '(' -> for (x in funcionArray.indices) {
+                if (funcionArray[x] == ')') {
+                    funcionArray.set(i, '-')
+                    funcionArray.set(x, '-')
+                    delimitadores.add(true)
+                    break
+                }
+
+            }
+
+            '[' -> for (x in funcionArray.indices) {
+                if (funcionArray[x] == ']') {
+                    funcionArray.set(i, '?')
+                    funcionArray.set(x, '?')
+                    delimitadores.add(true)
+                    break
+                }
+            }
+
+            '{' -> for (x in funcionArray.indices) {
+                if (funcionArray[x] == '}') {
+                    funcionArray.set(i, '¿')
+                    funcionArray.set(x, '¿')
+                    delimitadores.add(true)
+                    break
+                }
+            }
+        }
+    }
+
+    for (k in funcionArray.indices) {
+        when (funcionArray[k]) {
+            '(' -> estaBalanceada = false
+            ')' -> estaBalanceada = false
+            '[' -> estaBalanceada = false
+            ']' -> estaBalanceada = false
+            '{' -> estaBalanceada = false
+            '}' -> estaBalanceada = false
+        }
+    }
+
+    println("$funcion ¿está balanceada? $estaBalanceada")
+}
